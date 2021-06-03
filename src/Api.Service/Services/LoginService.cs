ee17330588ba77b4dc8032e0f1a9a52401b97208
Gabriel Jose <luckygb21@gmail.com>
2021-06-03 20:00:05 -0300

    - Autenticação com o Jwt

--- src/Api.Service/Services/LoginService.cs
+++ src/Api.Service/Services/LoginService.cs
@@ -1,18 +1,32 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Api.Domain.Dtos;
 using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Api.Domain.Repository;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Services
 {
     public class LoginService: ILoginService
     {
         private IUserRepository _repository;
+        private SigningConfigurations _signingConfigurations;
+        private TokenConfigurations _tokenConfigurations;
+        private  IConfiguration _configuration {get;}
 
-        public LoginService(IUserRepository repository)
+        public LoginService(IUserRepository repository, SigningConfigurations signingConfigurations,
+        TokenConfigurations tokenConfigurations, IConfiguration configuration)
         {
             _repository = repository;
+            _signingConfigurations = signingConfigurations;
+            _tokenConfigurations = tokenConfigurations;
+            _configuration = configuration;
         }
         public async Task<object> FindByLogin(LoginDto user)
         {
@@ -20,14 +34,52 @@ namespace Api.Service.Services
             if(user != null && !string.IsNullOrWhiteSpace(user.Email)){
                 baseuser = await _repository.FindByLogin(user.Email);
                 if(baseuser == null){
-                    return null;
+                    return new{
+                        authenticated = false,
+                        message = "Falha ao se autenticar"
+                    };
                 }else{
-                    return baseuser;
+                    ClaimsIdentity identity = new ClaimsIdentity(
+                        new GenericIdentity(baseuser.Email),
+                        new []{
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // jti o id do token
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.Email),
+                        }
+                    );
+                    DateTime createDate = DateTime.Now;
+                    DateTime expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds); // 60 segundos
+                    var handler = new JwtSecurityTokenHandler();
+                    var token =  CreateToken(identity, createDate, expirationDate, handler);
+                    return SuccessObject(createDate, expirationDate, token, user);
                 }
             }else{
                 return null;
             }
             //return await _repository.FindByLogin(user.Email);
+        }
+
+        private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler){
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor{
+                Issuer              = _tokenConfigurations.Issuer,
+                Audience            = _tokenConfigurations.Audicence,
+                SigningCredentials  = _signingConfigurations.SigningCredentials,
+                Subject             = identity,
+                NotBefore           = createDate,
+                Expires             = expirationDate,
+            });
+            var token = handler.WriteToken(securityToken);
+            return token;
+        }
+
+        private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, LoginDto user){
+            return new{
+                authenticated   = true,
+                created         = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                expiration      = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                acessToken      = token,
+                userName        = user.Email,
+                message         = "Usuario Logado com sucesso"
+            };
         }
     }
 }
